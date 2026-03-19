4288d57644a5b3b40ddbbe4d96525d1cc0f1851a
oyshi09etsecr <oyshi324@gmail.com>
2026-03-19 12:48:49 +0600

    Error Page Added Now

--- src/Components/Errror.jsx
+++ src/Components/Errror.jsx
@@ -1,16 +1,20 @@
+
+
 import React from 'react';
+import errror from '../assets/error-404.png'
 import { Link } from 'react-router-dom';
 
 const Errror = () => {
   return (
-   <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content text-center">
     <div className="max-w-md">
+
+      <img src={errror} alt="" />
       <h1 className="text-5xl font-bold">Opps</h1>
       <p className="py-6">
        this page not found
       </p>
-
      <Link to='/'  className='btn btn-primary'>
      Go back to Home
      </Link>
